5d24c6c5bb6527f34d63a28419d19e77fa1bb409
Kaleo <kaleocheng@gmail.com>
2023-06-23 23:23:25 +0800

    Add days command

--- pkgs/days/default.nix
+++ pkgs/days/default.nix
@@ -1,5 +1,13 @@
-{ writeShellScriptBin, neovim, dateutils }:
-
-writeShellScriptBin "days" ''
-  echo "Hello Days!"
-''
+{ writeShellApplication, neovim, writeTextFile }:
+let
+  daysConfig = writeTextFile {
+    name = "nvimrc";
+    text = (builtins.readFile ./nvimrc);
+  };
+in writeShellApplication {
+  name = "days";
+  runtimeInputs = [ neovim ];
+  text = ''
+    find ~/journal/"$(date +'%Y')"/* -mtime -180 | xargs nvim --cmd "source ${daysConfig}" -O
+  '';
+}
